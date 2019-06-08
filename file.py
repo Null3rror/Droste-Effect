34b35f41338d9f8bc1284bb895d5736ff0adec7c
NeoOreo <sepehr.razmyar@ut.ac.ir>
2019-06-08 11:27:43 +0330

    Everything works except tiling fadesaway

--- file.py
+++ file.py
@@ -4,8 +4,23 @@ import random
 
 repeats = int(input())
 
-def ROTATION(z, r, tetha):
-    return z * r * np.exp(1j * tetha)
+def Init(imgAddress):
+    img = cv2.imread(imgAddress, cv2.IMREAD_COLOR)
+    r = img.shape[0]
+    c = img.shape[1]
+    x = np.linspace(-1.0, 1.0, num=r, endpoint=True)
+    y = np.linspace(-1.0, 1.0, num=c, endpoint=True)
+    X, Y = np.meshgrid(x, y, indexing='ij')
+    Z = X + (1j * Y);
+    return img, r, c, Z
+
+def TILE(z, r1, r2, cOrig, j):
+    print(np.imag(z), j)
+    z = (1j * np.imag(z)) + np.mod(np.real(z) ,np.log(r2/r1)) * (int(j/cOrig) + 1)
+    return z;
+
+def ROTATION(z, f, tetha):
+    return z * f * np.exp(1j * tetha)
 
 def LOG(z, r1, r2):
     zLength = np.absolute(z)
@@ -13,61 +28,105 @@ def LOG(z, r1, r2):
         return(np.log(z/min(r1,r2)))
     return 0
 
-def WFunc(Z, r, c):
+def EXP(z):
+    return np.exp(z)
+
+def LogTransform(Z, r, c):
     Z1 = Z[:r, :c].copy()
     r1 = 0.2
     r2 = 0.9
-
     for i in range(r):
         for j in range(c):
             Z1[i][j] = LOG(Z1[i][j], r1, r2)
-            #Z1[i][j] = ROTATION(Z1[i][j], 1, np.pi/4)
+    return(Z1)
+
+def TILETransform(Z, r, c, cOrig):
+    Z1 = Z[:r, :c].copy()
+    r1 = 0.2
+    r2 = 0.9
+    for i in range(r):
+        for j in range(c):
+            Z1[i][j] = TILE(Z1[i][j], r1, r2, cOrig, j)
+    return(Z1)
+
+def ExpTransform(Z, r, c):
+    Z1 = Z[:r, :c].copy()
+    for i in range(r):
+        for j in range(c):
+            Z1[i][j] = EXP(Z1[i][j])
 
     return(Z1)
 
-def makeNew(W, wmax, x):
+def RotationTransformPi4(Z, r, c):
+    Z1 = Z[:r, :c].copy()
+    for i in range(r):
+        for j in range(c):
+            Z1[i][j] = ROTATION(Z1[i][j], 1, np.pi/4)
+    return(Z1)
+
+def RotationTransform(Z, r, c):
+    Z1 = Z[:r, :c].copy()
+    r1 = 0.2
+    r2 = 0.9
+    alpha = np.arctan( ( np.log(max(r2, r1) / min(r1, r2) ) / ( 2 * np.pi ) ) )
+    f = np.cos(alpha)
+    try:
+        for i in range(r):
+            for j in range(c):
+                Z1[i][j] = ROTATION(Z1[i][j], f, alpha)
+        return(Z1)
+    except IndexError:
+        print(i, j)
+def makeNewXY(W, wmax, x):
     return(np.multiply( np.add(np.divide(W, wmax), 1)  , x/2))
 
-def createImageAfterMapping(Xnew, Ynew, img, r, c, repeat):
-    newImg = np.zeros([r*repeat, c, 3])
-    for i in range(r):
-        for j in range(c*repeat):
-            for k in range(3):
-                if int(Xnew[i, j]) == r:
-                    Xnew[i, j] = r - 1
-                if int(Ynew[i, j]) == c * repeat:
-                    Ynew[i, j] = c * repeat - 1
-                newImg[int(Ynew[i, j])][int(Xnew[i, j])][k] = img[i, j % c, k]
-    return newImg
+def recreateImage(Xnew, Ynew, img, r, c, repeat):
+    try:
+        newImg = np.zeros([r , c*repeat, 3])
+        for i in range(r):
+            for j in range(c*repeat):
+                for k in range(3):
+                    if int(Xnew[i, j]) == c * repeat:
+                        Xnew[i, j] = c * repeat - 1
+                    if int(Ynew[i, j]) == r:
+                        Ynew[i, j] = r  - 1
+                    newImg[int(Ynew[i, j])][int(Xnew[i, j])][k] = img[i, j % c, k]
+        return newImg
+    except IndexError:
+        print(int(Ynew[i, j]), int(Xnew[i, j]), i, j, newImg.shape)
 
-img = cv2.imread('clock.jpg', cv2.IMREAD_COLOR)
-r = img.shape[0]
-c = img.shape[1]
-x = np.linspace(-1.0, 1.0, num=r, endpoint=True)
-y = np.linspace(-1.0, 1.0, num=c, endpoint=True)
-X, Y = np.meshgrid(x, y, indexing='ij')
-Z = X + (1j * Y);
+def CalculateXNewYNew(Z, r, c):
+    W = LogTransform(Z, r, c)
+    #W = RotationTransformPi4(Z, r, c)
+    Wx = np.real(W)
+    Wy = np.imag(W)
+    wxmax = np.absolute(Wx).max()
+    wymax = np.absolute(Wy).max()
+    Xnew = makeNewXY(Wx, wxmax, c)
+    Ynew = makeNewXY(Wy, wymax, r)
+    return Xnew, Ynew
 
-W = WFunc(Z, r, c)
-Wx = np.real(W)
-Wy = np.imag(W)
-wxmax = np.absolute(Wx).max()
-wymax = np.absolute(Wy).max()
-#print(wxmax1, wxmax2)
+def CalculateXNewYNewTILED(Z, r, c):
+    Z = np.tile(Z, (1, repeats))
+    print(Z.shape)
+    W = LogTransform(Z, r, c * repeats)
+    W = TILETransform(W, r, c * repeats, c)
+    #W = RotationTransform(W, r, c * repeats)
+    #W = ExpTransform(W, r, c * repeats)
+    Wx = np.real(W)
+    Wy = np.imag(W)
+    wxmax = np.absolute(Wx).max()
+    wymax = np.absolute(Wy).max()
+    Xnew = makeNewXY(Wx, wxmax, c * repeats)
+    Ynew = makeNewXY(Wy, wymax, r)
+    #print(Xnew.max(), Ynew.max())
+    return Xnew, Ynew
 
-
-Xnew = makeNew(Wx, wxmax, c)
-Ynew = makeNew(Wy, wymax, r)
-Xnew1 = np.tile(Xnew, repeats)
-Ynew1 = np.tile(Ynew, repeats)
-Ynew2 = Ynew1[:r, :c*repeats].copy()
+img, r, c, Z = Init('clock.jpg')
 if repeats != 1:
-    for i in range(r):
-        for j in range(repeats * c):
-            Ynew2[i][j] += int(j / c) * r
-            #print(j, int(j/repeats))
-print(Xnew1.shape, Ynew2.shape)
-newImg = createImageAfterMapping(Xnew1, Ynew2, img, r, c, repeats)
-#cv2.imshow('clock', newImg)
-cv2.imwrite("seplog.jpg", newImg);
+    Xnew, Ynew = CalculateXNewYNewTILED(Z, r, c)
+else:
+    Xnew, Ynew = CalculateXNewYNew(Z, r, c)
+newImg = recreateImage(Xnew, Ynew, img, r, c, repeats)
+cv2.imwrite("droste.jpg", newImg);
 #input()
