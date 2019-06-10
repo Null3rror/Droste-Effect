ba61f850ef8462176e71d3a711154f6cb96c6bb3
NeoOreo <sepehr.razmyar@ut.ac.ir>
2019-06-10 16:35:08 +0330

    better version

--- file.py
+++ file.py
@@ -6,6 +6,7 @@ repeats = int(input())
 r1 = 0.2
 r2 = 0.9
 
+
 def Init(imgAddress):
     img = cv2.imread(imgAddress, cv2.IMREAD_COLOR)
     r = img.shape[0]
@@ -17,13 +18,16 @@ def Init(imgAddress):
     return img, r, c, Z
 
 def TILE(W, r, c):
-    W1 = np.array([ [complex] * c * repeats] * r )
-    for i in range(repeats-1):
-        if i == 0:
-            W1 = np.concatenate([W, W + (i+1) * np.log(r2/r1)], axis = 1)
-        else:
-            W1 = np.concatenate([W1, W + (i+1) * np.log(r2/r1)], axis = 1)
-    return W1;
+    Wx = np.real(W); Wy = np.imag(W)
+    wxmax = np.absolute(Wx).max(); wymax = np.absolute(Wy).max()
+    Xnew = makeNewXY(Wx, wxmax, c); Ynew = makeNewXY(Wy, wymax, r)
+    Xnew = np.tile(Xnew, (repeats, 1)); Ynew = np.tile(Ynew, (repeats, 1))
+    for i in range(r *repeats):
+        for j in range(c):
+            Ynew[i][j] += int(i / c) * r
+    Xnew = bringBackXY(Xnew, wxmax, r)
+    Ynew = bringBackXY(Ynew, wymax, c)
+    return Xnew + (1j * Ynew)
 
 def ROTATION(z, f, tetha):
     return z * f * np.exp(1j * tetha)
@@ -37,85 +41,70 @@ def LOG(z):
 def EXP(z):
     return np.exp(z)
 
-def LogTransform(Z, r, c):
-    Z1 = Z[:r, :c].copy()
-    for i in range(r):
-        for j in range(c):
+def LogTransform(Z):
+    Z1 = Z.copy()
+    for i in range(Z.shape[0]):
+        for j in range(Z.shape[1]):
             Z1[i][j] = LOG(Z1[i][j])
     return(Z1)
 
-def ExpTransform(Z, r, c):
-    Z1 = Z[:r, :c].copy()
-    for i in range(r):
-        for j in range(c):
+def ExpTransform(Z):
+    Z1 = Z.copy()
+    for i in range(Z1.shape[0]):
+        for j in range(Z1.shape[1]):
             Z1[i][j] = EXP(Z1[i][j])
     return(Z1)
 
-def RotationTransformPi4(Z, r, c):
-    Z1 = Z[:r, :c].copy()
-    for i in range(r):
-        for j in range(c):
+def RotationTransformPi4(Z):
+    Z1 = Z.copy()
+    for i in range(Z.shape[0]):
+        for j in range(Z.shape[1]):
             Z1[i][j] = ROTATION(Z1[i][j], 1, np.pi/4)
     return(Z1)
 
-def RotationTransform(Z, r, c):
-    Z1 = Z[:r, :c].copy()
-    r1 = 0.2
-    r2 = 0.9
+def RotationTransform(Z):
+    Z1 = Z.copy()
     alpha = np.arctan( ( np.log(max(r2, r1) / min(r1, r2) ) / ( 2 * np.pi ) ) )
     f = np.cos(alpha)
-    for i in range(r):
-        for j in range(c):
+    for i in range(Z1.shape[0]):
+        for j in range(Z1.shape[1]):
             Z1[i][j] = ROTATION(Z1[i][j], f, alpha)
     return(Z1)
 
 def makeNewXY(W, wmax, x):
-    return(np.multiply( np.add(np.divide(W, wmax), 1)  , x/2))
+    return(np.multiply(np.add(np.divide(W, wmax), 1), x/2))
+def bringBackXY(W, wmax, x):
+    return(np.multiply((np.add(np.divide(W, x/2) ,-1)), wmax))
 
-def recreateImage(Xnew, Ynew, img, r, c, repeat):
-    newImg = np.zeros([r , c*repeat, 3])
-    for i in range(r):
-        for j in range(c*repeat):
-            for k in range(3):
-                if int(Xnew[i, j]) == c * repeat:
-                    Xnew[i, j] = c * repeat - 1
-                if int(Ynew[i, j]) == r:
-                    Ynew[i, j] = r  - 1
-                newImg[int(Ynew[i, j])][int(Xnew[i, j])][k] = img[i, j % c, k]
-    return newImg
+def recreateImage(X, Y, img, repeat):
+    print(X.shape, Y.shape, img.shape)
+    X = np.clip(X, 0, img.shape[1]-1)
+    Y = np.clip(Y, 0, img.shape[0]-1)
+    result = np.zeros(img.shape)
+    for j in range(Y.shape[1]):
+        for i in range(X.shape[0]):
+            result[int(Y[i][j])][int(X[i][j])] = img[i%img.shape[0]][j]
+    return result
 
 
-def CalculateXNewYNew(Z, r, c):
-    W = LogTransform(Z, r, c)
-    #W = RotationTransformPi4(Z, r, c)
-    Wx = np.real(W)
-    Wy = np.imag(W)
-    wxmax = np.absolute(Wx).max()
-    wymax = np.absolute(Wy).max()
-    Xnew = makeNewXY(Wx, wxmax, c)
-    Ynew = makeNewXY(Wy, wymax, r)
-    return Xnew, Ynew
 
 def CalculateXNewYNewTILED(Z, r, c):
     print(Z.shape)
-    W = LogTransform(Z, r, c)
+    W = LogTransform(Z)
     W1 = TILE(W, r, c)
-    print(W1.shape)
-    W1 = RotationTransform(W1, r, c * repeats)
-    W1 = ExpTransform(W1, r, c * repeats)
+    W1 = RotationTransform(W1)
+    W1 = ExpTransform(W1)
     Wx = np.real(W1)
     Wy = np.imag(W1)
     wxmax = np.absolute(Wx).max()
     wymax = np.absolute(Wy).max()
-    Xnew = makeNewXY(Wx, wxmax, c * repeats)
+    Xnew = makeNewXY(Wx, wxmax, c)
     Ynew = makeNewXY(Wy, wymax, r)
+
     return Xnew, Ynew
 
-img, r, c, Z = Init('clock.jpg')
-if repeats != 1:
-    Xnew, Ynew = CalculateXNewYNewTILED(Z, r, c)
-else:
-    Xnew, Ynew = CalculateXNewYNew(Z, r, c)
-newImg = recreateImage(Xnew, Ynew, img, r, c, repeats)
+img, r, c, Z = Init('pb.jpg')
+Xnew, Ynew = CalculateXNewYNewTILED(Z, r, c)
+newImg = recreateImage(Xnew, Ynew, img, repeats)
 cv2.imwrite("droste.jpg", newImg);
 input()
