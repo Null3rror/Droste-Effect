4abe71de4ac77bd5769c513fd0d032967b182efc
Null3rror <null3rr0r@protonmail.com>
2020-08-20 23:49:42 -0400

    quick fix pixels

--- droste.py
+++ droste.py
@@ -6,8 +6,9 @@ import os.path
 # Math explained here: http://www.josleys.com/article_show.php?id=82
 
 class Droste:
-    def __init__(self, fPath, repeats, r1, r2, xRange, yRange, resultFolderAddress):
-        self.repeats = repeats
+    def __init__(self, fPath, repeatsX, repeatsY, r1, r2, xRange, yRange, resultFolderAddress):
+        self.repeatsX = repeatsX
+        self.repeatsY = repeatsY
         self.r1 = min(r1, r2)
         self.r2 = max(r1, r2)
         self.inputImg = cv2.imread(fPath, cv2.IMREAD_UNCHANGED) #returns a matrix-like object
@@ -127,41 +128,41 @@ class Droste:
     def Calculate(self, Z, r, c):
         print(Z.shape)
         W1 = self.LogTransform(Z, r, c)
-        self.ReCreateImageAndSave(W1, "log-{}.{}-{}.{}.jpg".format(0, self.r1, self.r2, self.repeats), r, c, 1, 1)
+        # self.ReCreateImageAndSave(W1, "log-{}.{}-{}.{}-{}.jpg".format(0, self.r1, self.r2, 1, 1), r, c, 1, 1)
 
         W1 = self.RotationTransform(W1, r, c, 1, 1)
-        self.ReCreateImageAndSave(W1, "rot-{}.{}-{}.{}.jpg".format(0, self.r1, self.r2, self.repeats), r, c, 1, 1)
+        # self.ReCreateImageAndSave(W1, "rot-{}.{}-{}.{}-{}.jpg".format(0, self.r1, self.r2, 1, 1), r, c, 1, 1)
 
-        repeatX = self.repeats
-        repeatY = 1
+        repeatX = self.repeatsX
+        repeatY = self.repeatsY
         W1 = self.TILE2(W1, r, c, repeatX, repeatY)
         print(W1.shape)
-        self.ReCreateImageAndSave(W1, "tile-{}.{}-{}.{}.jpg".format(0, self.r1, self.r2, self.repeats), r, c, repeatX, repeatY)
+        # self.ReCreateImageAndSave(W1, "tile-{}.{}-{}.{}-{}.jpg".format(0, self.r1, self.r2, repeatX, repeatY), r, c, repeatX, repeatY)
 
         W1 = self.ExponentTransform(W1, r, c, repeatX, repeatY)
-        self.ReCreateImageAndSave(W1, "final-{}.{}-{}.{}.jpg".format(0, self.r1, self.r2, self.repeats), r, c, repeatX, repeatY)
+        self.ReCreateImageAndSave(W1, "final-{}.{}-{}.{}-{}.jpg".format(0, self.r1, self.r2, repeatX, repeatY), r, c, repeatX, repeatY)
 
     def Calculate1(self, Z, r, c):
         print(Z.shape)
         # apply Ln(z/r1)
         W1 = self.LogTransform(Z, r, c)
-        self.ReCreateImageAndSave(W1, "log-{}.{}-{}.{}.jpg".format(1, self.r1, self.r2, self.repeats), r, c, 1, 1)
+        # self.ReCreateImageAndSave(W1, "log-{}.{}-{}.{}-{}.jpg".format(1, self.r1, self.r2, 1, 1), r, c, 1, 1)
 
         # choose how many tiles we want to add
-        repeatX = self.repeats
-        repeatY = 1
-        # # apply Tile
+        repeatX = self.repeatsX
+        repeatY = self.repeatsY
+        # apply Tile
         W1 = self.TILE(W1, r, c, repeatX, repeatY)
         print(W1.shape)
-        self.ReCreateImageAndSave(W1, "tile-{}.{}-{}.{}.jpg".format(1, self.r1, self.r2, self.repeats), r, c, repeatX, repeatY)
-        #
-        # # apply Rotation
+        # self.ReCreateImageAndSave(W1, "rot-{}.{}-{}.{}-{}.jpg".format(1, self.r1, self.r2, repeatX, repeatY), r, c, repeatX, repeatY)
+
+        # apply Rotation
         W1 = self.RotationTransform(W1, r, c, repeatX, repeatY)
-        self.ReCreateImageAndSave(W1, "rot-{}.{}-{}.{}.jpg".format(1, self.r1, self.r2, self.repeats), r, c, repeatX, repeatY)
+        # self.ReCreateImageAndSave(W1, "tile-{}.{}-{}.{}-{}.jpg".format(1, self.r1, self.r2, repeatX, repeatY), r, c, repeatX, repeatY)
 
         # apply exponent
         W1 = self.ExponentTransform(W1, r, c, repeatX, repeatY)
-        self.ReCreateImageAndSave(W1, "final-{}.{}-{}.{}.jpg".format(1, self.r1, self.r2, self.repeats), r, c, repeatX, repeatY)
+        self.ReCreateImageAndSave(W1, "final-{}.{}-{}.{}-{}.jpg".format(1, self.r1, self.r2, repeatX, repeatY), r, c, repeatX, repeatY)
 
 
     def makeNewXY(self, W, wmax, x):
@@ -186,23 +187,61 @@ class Droste:
         :param repeat:
         :return: A matrix representing the pic in the new coordinates
         '''
-        newImg = np.zeros([r * repeatX, c * repeatY, 3])
+        # newImg = np.zeros([r * repeatX, c * repeatY, 3])
+        imgList = img.tolist()
+        XnewList = Xnew.tolist()
+        YnewList = Ynew.tolist()
+        newImgList = [[[0, 0, 0] for j in range(c * repeatY)] for i in range(r * repeatX)]
         for i in range(r * repeatX):
             for j in range(c * repeatY):
-                if int(Xnew[i, j]) == c * repeatY:
-                    Xnew[i, j] = c * repeatY - 1
-                if int(Ynew[i, j]) == r * repeatX:
-                    Ynew[i, j] = r * repeatX - 1
-                newImg[int(Ynew[i, j])][int(Xnew[i, j])] = img[i % r, j % c] # extract RGB from inputImage
-
+                if int(XnewList[i][j]) == c * repeatY:
+                    XnewList[i][j] = c * repeatY - 1
+                if int(YnewList[i][j]) == r * repeatX:
+                    YnewList[i][j] = r * repeatX - 1
+                newImgList[int(YnewList[i][j])][int(XnewList[i][j])] = imgList[i % r][j % c][:3] # extract RGB from inputImage
+        newImg = np.array(newImgList)
         return newImg
+
+    # I know this is overkill
+    # need to find a opencv equivalent
+    # if you know tell me plz :)
+    def FixBlackPixels(self, img, r, c, repeatX, repeatY):
+        global image, limitR, limitC
+        black = [0,0,0]
+        image = img
+        limitR = r * repeatX - 1
+        limitC = c * repeatY - 1
+        imgList = img.tolist()
+        for i in range(limitR):
+            for j in range(limitC):
+                if imgList[i][j] == black:
+                    n = 0
+                    temp = [0,0,0]
+                    # diagonal screws the result
+                    for u, v in [(1, 0), (-1, 0), (2, 0), (-2, 0), (0, 1), (0, -1), (0, 2), (0, -2)]:
+                        if not (u == 0 and v == 0):
+                            if 0 <= i + u and i + u <= limitR and 0<= j + v and j + v <= limitC:
+                                if imgList[i + u][j + v] != black:
+                                    n += 1
+                                    temp = [imgList[i + u][j + v][k] + temp[k] for k in range(3)]
+                    if n != 0:
+                        imgList[i][j] = [temp[k] / n for k in range(3)]
+
+        img = np.array(imgList)
+        return img
 
 
 
     def ReCreateImageAndSave(self, W, imgTitle, r, c, repeatX, repeatY):
         xNew, yNew = self.GetNewXY(W, r, c, repeatX, repeatY)
         img = self.ReCreateImage(xNew, yNew, self.inputImg, r, c, repeatX, repeatY)
+        img = self.FixBlackPixels(img, r, c, repeatX, repeatY)
+
+        # img[blackPixels] = cv2.blur(img.astype(np.float32), (5, 5))
+        # img = cv2.medianBlur(img.astype(np.float32), 5)
         cv2.imwrite(self.resultFolderAddress + '/' + imgTitle, img)
+
+
 
 def GetCommandLineArgs():
     '''
@@ -212,21 +251,24 @@ def GetCommandLineArgs():
              method must be 0 or 1
              repeats must be positive and odd
     '''
-    if len(sys.argv) == 4:
+    if len(sys.argv) == 5:
         fPath = sys.argv[1] # Get input File name
         method = int(sys.argv[2])
-        repeats = int(sys.argv[3])
+        repeatsX = int(sys.argv[3])
+        repeatsY = int(sys.argv[4])
         if not os.path.isfile(fPath):
             sys.exit("Input picture does not exist")
         if not (method == 0 or method == 1):
             sys.exit("Method must be 0 or 1")
-        if repeats <= 0 or repeats & 1 == 0:
+        if repeatsX <= 0 or repeatsX & 1 == 0:
             sys.exit("Repeats must be positive and odd")
-        return fPath, method, repeats
+        if repeatsY <= 0 or repeatsY & 1 == 0:
+            sys.exit("Repeats must be positive and odd")
+        return fPath, method, repeatsX, repeatsY
     else:
         sys.exit("Need only two command line argument and that's the address of input picture and method number\nMethod number is either 0 or 1\nRepeats must be positive and odd")
 
-def CreateOutputFolder(inputImagePath, repeats, method, r1, r2):
+def CreateOutputFolder(inputImagePath, repeatsX, repeatsY, method, r1, r2):
     '''
     :param inputImagePath: address of our input image
     :param repeats:        number of repeats
@@ -235,24 +277,24 @@ def CreateOutputFolder(inputImagePath, repeats, method, r1, r2):
     inputImageName = inputImagePath.split('/')[-1] # extract input image name
     outputFolder = "OutputImages/"        # pictures will be stored
 
-    resultFolderAddress = outputFolder + inputImageName + "-{}.{}-{}.{}.jpg".format(method, r1, r2, repeats) # the folder that our code is stored in
+    resultFolderAddress = outputFolder + inputImageName + "-{}.{}-{}.{}-{}.jpg".format(method, r1, r2, repeatsX, repeatsY) # the folder that our code is stored in
     if not os.path.exists(resultFolderAddress): # create the result folder
         os.makedirs(resultFolderAddress)
     return resultFolderAddress
 
 def main():
-    inputImagePath, method, repeats = GetCommandLineArgs()
+    inputImagePath, method, repeatsX, repeatsY = GetCommandLineArgs()
     # We have two methods of calculating the result
     # 0: Ln -> Rotate -> Tile   -> exponent
     # 1: Ln -> Tile   -> Rotate -> exponent
 
     r1      = 0.2      # inner circle radius
-    r2      = 0.99    # outer circle radius
+    r2      = 0.9    # outer circle radius
 
-    resultFolderAddress = CreateOutputFolder(inputImagePath, repeats, method, r1, r2)
+    resultFolderAddress = CreateOutputFolder(inputImagePath, repeatsX, repeatsY, method, r1, r2)
 
-    d = Droste(inputImagePath, repeats, r1, r2, -1, 1, resultFolderAddress)
-
+    d = Droste(inputImagePath, repeatsX, repeatsY, r1, r2, -1, 1, resultFolderAddress)
+    print('be patient plz')
     d.Transform(method)
 
 
